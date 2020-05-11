834e0401d5ca2886ddbcf11f1f95cf85453d3425
dhananjaybhirud <dhananjay.b@mobinexttech.com>
2020-05-12 00:29:02 +0530

    - form validation done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  enquiryForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.enquiryForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      number: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', Validators.required],
+      language: ['', Validators.required],
+      message: [''],
+      terms: ['', Validators.required]
+    });
+  }
+
+  onSubmit(){
+   console.log(this.enquiryForm)
   }
 
 }
